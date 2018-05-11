d0a8ba5fb8aa5861b99c4062421d9470b3958548
geren <15233866884@.com>
2018-05-11 11:27:26 +0800

    Add Log02

--- app/src/main/java/com/spl/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/spl/dell/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("TTT","15233866887");
+        Log.d("TTT","15233866887");
 
     }
 }
